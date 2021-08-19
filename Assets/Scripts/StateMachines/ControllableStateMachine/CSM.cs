a2c5d029baeea45fe85432610d7ea965e4ec7363
Pqka <53070266+Zapaway@users.noreply.github.com>
2021-08-19 04:16:50 -0400

    Simplified State Machines
    Shortened down CSM and removed QSM in favor of a QuantumSystemInfo class.

--- Assets/Scripts/StateMachines/ControllableStateMachine/CSM.cs
+++ Assets/Scripts/StateMachines/ControllableStateMachine/CSM.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Cysharp.Threading.Tasks;
 
 namespace StateMachines.CSM {
@@ -9,22 +6,5 @@ namespace StateMachines.CSM {
     /// There should only be one of these.
     /// All states can be disabled via disabling specific stage input.
     /// </summary>
-    public sealed class CSM : StateMachine<CSMState>
-    {
-        private CSMState _currState;
-        public override CSMState CurrentState => _currState;
-
-        public override async UniTask InitializeState(CSMState initState)
-        {
-            _currState = initState;
-            await _currState.Enter();
-        }
-        public override async UniTask ChangeState(CSMState newState)
-        {
-            await _currState.Exit();
-
-            _currState = newState;
-            await _currState.Enter();
-        }
-    }
+    public sealed class CSM : StateMachine<CSMState> {}
 }
--- Assets/Scripts/StateMachines/StateMachine.cs
+++ Assets/Scripts/StateMachines/StateMachine.cs
@@ -2,23 +2,34 @@ using Cysharp.Threading.Tasks;
 
 namespace StateMachines {
     /// <summary>
-    /// Base class for all state machines.
+    /// Base class for all singular state machines.
     /// </summary>
     public abstract class StateMachine<T> where T : IState
     {
+        protected T _currState;
+
         /// <value>
         /// Current state of the state machine.
         /// </value>
-        public abstract T CurrentState {get;}
+        public T CurrentState => _currState;
 
         /// <summary>
         /// Initalize the state.
         /// </summary>
-        public abstract UniTask InitializeState(T initState);
+        public async virtual UniTask InitializeState(T initState) {
+            _currState = initState;
+            await _currState.Enter();
+        }
+
         /// <summary>
         /// Change the state.
         /// </summary>
-        public abstract UniTask ChangeState(T newState);
+        public async virtual UniTask ChangeState(T newState) {
+            await _currState.Exit();
+
+            _currState = newState;
+            await _currState.Enter();
+        }
     }
 }
 
