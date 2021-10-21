eda64a4916237dbf179ac5616286392bdf3512e2
Pqka <53070266+Zapaway@users.noreply.github.com>
2021-10-21 00:04:18 -0400

    Worked on QDP

--- Assets/Scripts/UI/PieChartScript.cs
+++ Assets/Scripts/UI/PieChartScript.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PieChartScript : MonoBehaviour
 {
     [SerializeField] private Image[] _pieLayers;
-    private float[] _values;
-    public float[] Values {
-        get => Values;
-        set {
-            // automatically change the pie chart to reflect the changes in value
-            
+
+    /// <summary>
+    /// Automatically change the pie chart to reflect the changes in value.
+    /// </summary>
+    public void SetValues(float[] values) {
+        float total = values.Sum();
+        float currSum = 0;
+
+        for (int i = 0; i < values.Length; ++i) {
+            currSum += values[i];
+            _pieLayers[i].fillAmount = currSum/total;
         }
     }
 }
