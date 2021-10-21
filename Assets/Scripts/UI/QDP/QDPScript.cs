88d2c00a75334a7297cee46f77b3225e587e1502
Kenneth Chap <53070266+Zapaway@users.noreply.github.com>
2021-10-21 14:49:16 -0400

    Added More to QDP's Script

--- Assets/Scripts/UI/QDP/QDPScript.cs
+++ Assets/Scripts/UI/QDP/QDPScript.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class QDPScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace UIScripts.QDP {
+    /// <summary>
+    /// Script for interactions with the qubit display panel (QDP).
+    /// </summary>
+    public sealed class QDPScript : MonoBehaviour
     {
-        
-    }
+        // qubit desc
+        [SerializeField] private RawImage _qubitRepRawImage;
+        [SerializeField] private TextMeshProUGUI _qubitIndexText;
+        [SerializeField] private TextMeshProUGUI _qubitStateText;
 
-    // Update is called once per frame
-    void Update()
-    {
+        // probabilites 
+        [SerializeField] private PieChartScript _pieChart;
+        [SerializeField] private TextMeshProUGUI _groundStateProbText;
+        [SerializeField] private TextMeshProUGUI _excitedStateProbText;
+
         
     }
 }
+
